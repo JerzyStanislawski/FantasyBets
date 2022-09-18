b5625e5b26018facd66719e4cee8dde043224bba
JerzyStanislawski <stanislawski.jerzy@hotmail.com>
2022-09-18 09:49:12 +0200

    identity

--- FantasyBets/FantasyBets/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ FantasyBets/FantasyBets/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,54 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FantasyBets.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public string ReturnUrl { get; set; } = null!;
+
         public void OnGet()
         {
+            ReturnUrl = Url.Content("~/");
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(ReturnUrl);
+                }
+            }
+
+            return Page();
+        }
+    }
+
+    public class InputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
     }
 }
--- FantasyBets/FantasyBets/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FantasyBets/FantasyBets/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,62 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace FantasyBets.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; } = null!;
+
+        public string ReturnUrl { get; set; } = null!;
+
         public void OnGet()
         {
+            ReturnUrl = Url.Content("~/");
+        }
+        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var identity = new IdentityUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email
+                };
+                var result = await _userManager.CreateAsync(identity, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(identity, false);
+                    return LocalRedirect(ReturnUrl);
+                }
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = null!;
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; } = null!;
         }
     }
 }
