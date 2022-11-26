e76d7ba1eee09febead329cdcce998caa56d9e5b
JerzyStanislawski <stanislawski.jerzy@hotmail.com>
2022-11-27 00:34:10 +0100

    Top bets

--- FantasyBets/FantasyBets/Extensions/GameExtensions.cs
+++ FantasyBets/FantasyBets/Extensions/GameExtensions.cs
@@ -10,10 +10,14 @@ namespace FantasyBets.Extensions
         {
             _cet = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
         }
+
         public static bool Started(this Game game)
         {
             var cetNow = TimeZoneInfo.ConvertTime(DateTime.UtcNow, _cet);
             return cetNow > game.Time;
         }
+
+        public static string Link(this Game game, Configuration config)
+            => $"{config.Feeds.GameStatsGuiPrefix}/{config.CurrentSeasonSymbol}/{game.HomeTeam.Name.Replace(' ', '-')}-{game.AwayTeam.Name.Replace(' ', '-')}/{config.CurrentSeasonCode}/{game.Code}/#boxscore";
     }
 }
---
+++ FantasyBets/FantasyBets/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Mail;
+
+namespace FantasyBets.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToUserName(this string email)
+        {
+            try
+            {
+                var address = new MailAddress(email);
+
+                return address.User;
+            }
+            catch
+            {
+                return "<unknown>";
+            }
+        }
+    }
+}
